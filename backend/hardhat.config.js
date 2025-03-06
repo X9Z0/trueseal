13c7c61548e282eedc664e1a53495613a9219292
X9Z0 <x9z0cofd@gmail.com>
2025-03-07 00:46:51 +0530

    hardhat config

---
+++ backend/hardhat.config.js
@@ -0,0 +1,22 @@
+// /** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+    // Add other networks as needed (e.g., testnets, mainnet)
+  },
+  paths: {
+    artifacts: "./artifacts",
+    cache: "./cache",
+    sources: "./contracts",
+    tests: "./test",
+  },
+};
